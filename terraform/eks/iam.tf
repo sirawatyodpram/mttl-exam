8560c9a5a69780fbd85f62467fc2e790e34310b7
SirawatY <sirawaty@scg.com>
2023-12-27 16:56:20 +0700

    add s3 and sqs policy

--- terraform/eks/iam.tf
+++ terraform/eks/iam.tf
@@ -1,30 +1,10 @@
-# module "iam_assumable_role_with_eks_oidc" {
-#   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-
-#   create_role = true
-
-#   role_name = "role-with-oidc"
-
-#   tags = {
-#     Role = "role-with-oidc"
-#   }
-
-#   provider_url = module.eks.oidc_provider_arn
-
-#   role_policy_arns = [
-#     "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
-#   ]
-#   number_of_role_policy_arns = 1
-# }
-
 module "iam_assumable_role_with_eks_oidc" {
   source      = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
 
   role_name   = "role-with-oidc"
 
   role_policy_arns = {
-    s3_policy = "arn:aws:iam::aws:policy/AmazonS3FullAccess",
-    sqs_policy = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
+    policy = "${module.exam_app_iam_policy.arn}",
   }
 
   # attach_vpc_cni_policy = true
@@ -36,4 +16,39 @@ module "iam_assumable_role_with_eks_oidc" {
       namespace_service_accounts = ["demo:demo-sa"]
     }
   }
+}
+
+module "exam_app_iam_policy" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
+
+  name        = "exam_app_iam_policy"
+  path        = "/"
+  description = "exam_app_iam_policy"
+
+  policy = <<EOF
+{
+	"Version": "2012-10-17",
+	"Statement": [
+		{
+			"Sid": "s3",
+			"Effect": "Allow",
+			"Action": [
+				"s3:PutObject",
+				"s3:GetObject"
+			],
+			"Resource": "arn:aws:s3:::my-web-assets/*"
+		},
+		{
+			"Sid": "sqs",
+			"Effect": "Allow",
+			"Action": [
+				"sqs:DeleteMessage",
+				"sqs:ReceiveMessage",
+				"sqs:SendMessage"
+			],
+			"Resource": "arn:aws:sqs:ap-southeast-1:123456789123:lms-import-data"
+		}
+	]
+}
+EOF
 }
